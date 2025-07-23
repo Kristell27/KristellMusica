2823bfcdc05ad293780e5d97613e9e552e92f5f7
Kristell27 <kristell.martinezb@pca.edu.co>
2025-07-23 00:29:20 -0500

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,6 +15,10 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'intro',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   }
 ];
 
